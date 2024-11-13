db9d845b9236f653e88f1e7405e3082290289953
Nordine Bittich <contact@bittich.be>
2024-11-13 08:03:31 +0100

    remove unused

--- src/common/constant.rs
+++ src/common/constant.rs
@@ -1,14 +1,11 @@
-pub const CORS_ALLOW_ORIGIN: &str = "CORS_ALLOW_ORIGIN";
 pub const BODY_SIZE_LIMIT: &str = "BODY_SIZE_LIMIT";
 pub const SERVICE_HOST: &str = "SERVICE_HOST";
 pub const SERVICE_PORT: &str = "SERVICE_PORT";
-pub const SERVICE_CONFIG_VOLUME: &str = "SERVICE_CONFIG_VOLUME";
-pub const SERVICE_DATA_VOLUME: &str = "SERVICE_DATA_VOLUME";
 pub const SERVICE_APPLICATION_NAME: &str = "SERVICE_APPLICATION_NAME";
 pub const FILE_SERVICE_COLLECTION_NAME: &str = "FILE_SERVICE_COLLECTION_NAME";
 pub const TEMPL_SERVICE_COLLECTION_NAME: &str = "TEMPL_SERVICE_COLLECTION_NAME";
-pub const PUBLIC_TENANT: &str = "public";
 pub const SHARE_DRIVE_PATH: &str = "SHARE_DRIVE_PATH";
 pub const THUMB_HEIGHT: &str = "THUMB_HEIGHT";
 pub const THUMB_WIDTH: &str = "THUMB_WIDTH";
 pub const TZ: &str = "TZ";
+pub const PUBLIC_TENANT: &str = "public";
