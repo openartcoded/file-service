974e1213a630777cdc8fbb43fc532afcd0d3ddba
Nordine Bittich <contact@bittich.be>
2025-11-11 13:06:11 +0100

    add extra flags

--- src/template/render.rs
+++ src/template/render.rs
@@ -214,6 +214,7 @@ pub fn get_chromium_tab() -> Result<Arc<Tab>, Box<dyn Error>> {
                     //  OsStr::new("--disable-setuid-sandbox"),
                     OsStr::new("--allow-file-access-from-files"),
                     OsStr::new("--enable-local-file-accesses"),
+                    OsStr::new("--disable-pdf-tagging"),
                     OsStr::new("--disable-features=IsolateOrigins,site-per-process"),
                     OsStr::new("--default-background-color=00000000"),
                     OsStr::new("--disable-dev-shm-usage"),
